8a0e053b0fbf3be789e72768e9bd9451690673f0
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-05 11:09:55 +0300

    fixing env for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,16 @@
 import pathlib
+
 import sys
-import os
 import pytest
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+ROOT_DIR = pathlib.Path(__file__).parent.parent
+sys.path.insert(0, str(ROOT_DIR))
 
 
 @pytest.fixture(scope="session", autouse=True)
 def set_env():
-    env_file = pathlib.Path(__file__).parent.parent / ".env"
+    env_file = ROOT_DIR / ".env"
     if env_file.exists():
         from dotenv import load_dotenv
 
